dfc5b0a475231898772e391426899aaeb7a2b93a
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-07-27 23:30:49 +0600

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
     'next/core-web-vitals',
     'prettier',
     '@typescript-eslint/recommended',
-    'plugin:storybook/recommended',
     'eslint:recommended',
-    'plugin:import/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import/recommended', 'storybook/recommended'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
