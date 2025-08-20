a3441b3af73921eac3f32c4e5f01880b30fe8380
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-20 15:11:54 +0600

    chore: removed hono server

--- src/app/api/[...route]/route.ts
+++
@@ -1,8 +0,0 @@
-import { app } from '#/server'
-import { handle } from 'hono/vercel'
-
-export const dynamic = 'force-dynamic'
-export const GET = handle(app)
-export const POST = handle(app)
-export const PATCH = handle(app)
-export const DELETE = handle(app)
